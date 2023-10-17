554f84b52c90a914ea18334632a4790dc3543466
asad <ranaasad559921@gmail.com>
2023-10-17 16:51:01 +0500

    change some agriculture page css

--- src/components/Agriculture/AgricultureOne.jsx
+++ src/components/Agriculture/AgricultureOne.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import img1 from "../../../src/images/agricultural-runoff.jpg";
+import img1 from "../../../src/images/what-is-agriculture.png";
 
 function AgricultureOne() {
   return (
     <section className="flex items-center flex-col gap-8 lg:flex-row justify-center mt-[100px] mb-[50px]  px-[30px] md:px-[60px] lg:px-[80px] xl:px-[100px]">
       {/* Left Column - Heading and Paragraph */}
       <div className="lg:w-1/2  my-10 lg:my-0">
-        <h2 className=" mb-6 tracking-normal text-[#316D69] leading font-bold text-4xl font-montserrat">
+        <h2 className=" mb-8 text-[#316D69] leading-10 font-bold text-2xl md:text-3xl lg:text-4xl font-montserrat">
           What is <span className="text-[#bab220]">Agriculture</span>
         </h2>
-        <p className="text-lg text-[#3c3c3c] font-normal font-poppins leading-7 mb-6">
+        <p className="text-sm md:text-base lg:text-lg text-[#3c3c3c] font-normal font-poppins leading-7 mb-6">
           Agriculture contributes to carbon sequestration, the process of
           extracting carbon dioxide from the atmosphere and storing it in the
           soil and vegetation. This can be achieved through various methods,
           including:
         </p>
-        <ul className=" list-inside gap-4 font-poppins  text-[#3c3c3c] font-normal text-base leading-7 mb-6 ml-4">
+        <ul className=" list-inside gap-4 font-poppins  text-[#3c3c3c] font-normal text-sm md:text-base lg:text-lg leading-7 mb-6 ml-4">
           <li className="mt-2 ">
             <span className="text-[#316D69] font-bold ">
               {" "}
@@ -45,7 +45,7 @@ function AgricultureOne() {
           </li>
         </ul>
 
-        <p className="text-lg leading-7 text-[#3c3c3c] font-normal font-poppins  mb-6">
+        <p className="text-sm md:text-base lg:text-lg leading-7 text-[#3c3c3c] font-normal font-poppins  mb-6">
           These practices not only aid in mitigating climate change by removing
           carbon from the atmosphere but also enhance soil health and fertility,
           elevate crop yields, and offer additional environmental benefits.
@@ -54,7 +54,7 @@ function AgricultureOne() {
 
       {/* Right Column - Image */}
       <div className="lg:w-1/2 overflow-hidden rounded-lg shadow-3xl transform hover:rotate-2 transition-transform duration-300">
-        <img src={img1} alt="Example" className="w-full h-auto" />
+        <img src={img1} alt="Example" className="w-full h-full" />
       </div>
     </section>
   );
--- src/components/Agriculture/AgricultureTwo.jsx
+++ src/components/Agriculture/AgricultureTwo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img1 from "../../../src/images/agriculture-trac.jpg";
+import img1 from "../../../src/images/agriculture-significance.png";
 
 function AgricultureTwo() {
   return (
@@ -9,20 +9,20 @@ function AgricultureTwo() {
         <img
           src={img1}
           alt="Example"
-          className="w-full h-[400px] transform hover:scale-105 transition-transform duration-300"
+          className="w-full h-full transform hover:scale-105 transition-transform duration-300"
         />
       </div>
 
       {/* Right Column - Heading and Paragraph */}
       <div className="lg:w-1/2 px-8 md:px-10 lg:px-12 my-10 lg:my-0">
-        <h2 className=" mb-3 tracking-normal text-[#316D69] font-bold text-4xl leading-7 font-montserrat">
+        <h2 className=" mb-8 text-[#316D69] font-bold text-2xl md:text-3xl lg:text-4xl leading-10 font-montserrat">
           The Significance of{" "}
           <span className="text-[#bab220]">Agriculture</span>
         </h2>
-        <p className="font-normal text-[#3c3c3c] font-poppins leading-7 text-lg mt-6 mb-4">
+        <p className="font-normal text-[#3c3c3c] font-poppins leading-7 text-sm md:text-base lg:text-lg mb-4">
           Agriculture holds pivotal importance for various reasons:
         </p>
-        <ul className="list-none list-inside text-[#3c3c3c] font-normal text-lg leading-7 mb-8 font-poppins">
+        <ul className="list-none list-inside text-[#3c3c3c] font-normal text-sm md:text-base lg:text-lg leading-7 mb-8 font-poppins">
           <li className=" mt-2">
             <span className="text-[#bab220] font-bold">1. Food Security: </span>
             It provides essential sustenance to nourish human life and sustain
