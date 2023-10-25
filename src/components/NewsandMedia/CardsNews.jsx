85f37810b58729990b5a00569b2d1a155bb980a3
asad <ranaasad559921@gmail.com>
2023-10-25 22:40:57 +0500

    change some css and image sin all component news and media page

--- src/components/NewsandMedia/CardsNews.jsx
+++ src/components/NewsandMedia/CardsNews.jsx
@@ -5,14 +5,14 @@ import uae from "../../../src/images/media-thumb-2.jpg";
 const CardsNews = () => {
   return (
     <div className="mx-auto px-[30px] md:px-[60px] lg:px-[80px] xl:px-[100px] mb-[100px] ">
-      <div className="cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 w-[60%] h-[80%] justify-center items-center m-auto">
+      <div className="cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 xl:w-[60%] xl:h-[80%] justify-center items-center m-auto">
         <a href="/mou-uganda">
           <div className="inner-card flex flex-col text-center items-center hover:shadow-lg hover:shadow-[#316D69] p-8  rounded-lg">
             <img src={mou} alt="cards-images" className="h-[80%] w-[100%] " />
-            <h6 className="uppercase text-sm md:text-base lg:text-lg text-[#316D69] font-bold leading-7 lg:leading-9	font-montserrat mt-4">
+            <h6 className="uppercase text-sm md:text-base xl:text-lg text-[#316D69] h-[40px] md:h-[20px] lg:h-[20px] xl:h-[20px] font-bold leading-7 xl:leading-9	font-montserrat mt-4">
               MoU Ceremony
             </h6>
-            <p className="text-sm md:text-base lg:text-lg leading-5 lg:leading-7 text-[#3c3c3c] font-normal font-poppins mt-2">
+            <p className="text-sm md:text-base xl:text-lg leading-5 xl:leading-7 text-[#3c3c3c] h-[40px] font-normal font-poppins mt-2">
               Between GEP and Uganda Government
             </p>
           </div>
@@ -20,10 +20,10 @@ const CardsNews = () => {
         <a href="/uae-project">
           <div className="inner-card flex flex-col text-center items-center hover:shadow-lg hover:shadow-[#316D69] p-8  rounded-lg">
             <img src={uae} alt="cards-images" className="h-[80%] w-[100%] " />
-            <h6 className="uppercase text-sm md:text-base lg:text-lg text-[#316D69] font-bold leading-7 lg:leading-9	font-montserrat mt-4">
+            <h6 className="uppercase text-sm md:text-base xl:text-lg text-[#316D69] h-[40px] md:h-[20px] lg:h-[20px] xl:h-[20px] font-bold leading-7 xl:leading-9	font-montserrat mt-4">
               UAE project developers
             </h6>
-            <p className="text-sm md:text-base lg:text-lg leading-5 lg:leading-7 text-[#3c3c3c] font-normal font-poppins mt-2">
+            <p className="text-sm md:text-base xl:text-lg leading-5 xl:leading-7 text-[#3c3c3c] h-[40px] font-normal font-poppins mt-2">
               Gear up for expansion pitch ahead of COP28
             </p>
           </div>
--- src/components/NewsandMedia/MediaCoverage.jsx
+++ src/components/NewsandMedia/MediaCoverage.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const MediaCoverage = () => {
   return (
     <div className="flex flex-col pt-[30px] md:pt-[50px] lg:pt-[50px] pb-[50px] gap-6  px-[30px] md:px-[60px] text-center   lg:px-[80px] xl:px-[100px] ">
-      <h2 className="text-[#316D69] mb-4 md:mb-4 lg:mb-6 text-2xl md:text-3xl lg:text-4xl leading-7 lg:leading-10 font-montserrat font-bold">
+      <h2 className="text-[#316D69] mb-2 md:mb-4 xl:mb-6 text-2xl md:text-3xl xl:text-4xl leading-7 xl:leading-10 font-montserrat font-bold">
         News and Media Coverage
       </h2>
-      <p className="text-[#3c3c3c] font-poppins text-xl font-normal leading-5 lg:leading-7">
+      <p className="text-[#3c3c3c] font-poppins text-sm md:text-base xl:text-lg font-normal leading-5 xl:leading-7">
         Check out the latest news and updates from Green Economy Partnership -
         stay informed on how we are
         <br /> making real, sustainable progress.
