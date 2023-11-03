e889349d7393ecfd94765e65b6310f8ecc015388
asad <ranaasad559921@gmail.com>
2023-11-03 18:19:53 +0500

    add some link in footer components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,10 @@ const mainfooter = () => {
       <div className="container-fluid flex bg-[#ffffff] justify-between lg:py-16 pb-4 pt-4  lg:pb-6 px-[30px] md:px-[60px] lg:px-[70px] xl:px-[80px] ">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-2  lg:space-y-28 space-y-24 flex-wrap lg:gap-6 row-footer sm:pb-6 pb-6">
           <div className="grid-cols-3 space-y-4 f-line2 lg:ml-4">
-            <img className="-ml-5" src={Footerlogo} alt="footerlogo" />
+            <a href="/">
+              {" "}
+              <img className="-ml-5" src={Footerlogo} alt="footerlogo" />
+            </a>
             <p className="text-[#3C3C3C] text-sm md:text-base lg:text-lg font-poppins font-normal leading-5 lg:leading-7">
               Our mission is to lead the way in developing and restoring
               forestry for carbon capture and ecosystem services, creating a
@@ -47,11 +50,25 @@ const mainfooter = () => {
               Quick Links
             </h4>
             <ul className="flex flex-col gap-2 text-sm md:text-base lg:text-lg font-medium font-montserrat leading-5 lg:leading-7 text-[#3C3C3C]">
-              <li className=" ">Home</li>
-              <li className="">About</li>
-              <li className="">Projects</li>
-              <li className="">Challenges</li>
-              <li className="">Contact</li>
+              <a href="/">
+                {" "}
+                <li className=" ">Home</li>
+              </a>
+              <a href="/our-story">
+                {" "}
+                <li className="">About</li>
+              </a>
+              <a href="/projects">
+                <li className="">Projects</li>
+              </a>
+              <a href="/challenges">
+                {" "}
+                <li className="">Challenges</li>
+              </a>
+              <a href="/contact-us">
+                {" "}
+                <li className="">Contact</li>
+              </a>
             </ul>
           </div>
 
@@ -60,11 +77,26 @@ const mainfooter = () => {
               Services
             </h4>
             <ul className="flex flex-col gap-2 text-[#3C3C3C] text-sm md:text-base lg:text-lg  font-medium font-montserrat leading-5 lg:leading-7">
-              <li className="  ">Wind and Solar</li>
-              <li className="">Agriculture</li>
-              <li className="">Forestry</li>
-              <li className="">Blue Carbon</li>
-              <li className="">Anaerobic Digestion</li>
+              <a href="/wind-and-solar">
+                {" "}
+                <li className="  ">Wind and Solar</li>
+              </a>
+              <a href="/agriculture">
+                {" "}
+                <li className="">Agriculture</li>
+              </a>
+              <a href="/forestry">
+                {" "}
+                <li className="">Forestry</li>
+              </a>
+              <a href="/blue-carbon">
+                {" "}
+                <li className="">Blue Carbon</li>
+              </a>
+              <a href="/anaerobic-digestion">
+                {" "}
+                <li className="">Anaerobic Digestion</li>
+              </a>
             </ul>
           </div>
 
